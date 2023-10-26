75b5cee9d61746d5efd68e7cef11e96a4b5c2fe6
cxxi <l.a_lumet@yahoo.fr>
2023-10-26 17:02:27 +0200

    DEV - add install process

--- phpv.sh
+++ phpv.sh
@@ -31,24 +31,23 @@ Execute()
 
 		sudo update-alternatives --set php "/usr/bin/php$PHP_CLI_VERSION" 1> /dev/null
 
-		sudo a2disconf php*.*-fpm 1> /dev/null
-		sudo a2enconf "php$PHP_APACHE2_VERSION-fpm" 1> /dev/null
+		sudo a2disconf --quiet php*.*-fpm 1> /dev/null
+		sudo a2enconf --quiet "php$PHP_APACHE2_VERSION-fpm" 1> /dev/null
 		sudo systemctl restart apache2 1> /dev/null
 
 		sudo composer self-update --$COMPOSER_VERSION --quiet
 
 	fi 
 
-	echo "\n+--------------------+---------+"
-	echo "| PHP_CLI | $PHP_CLI_VERSION | | PHP_APACHE2 | $PHP_APACHE2_VERSION | | COMPOSER | $COMPOSER_VERSION |" | xargs -n5 printf '%-2s  %-15s  %-2s  %-5s %-2s\n'
-	echo "+--------------------+---------+\n"
+	printf "\n+--------------------+---------+\n"
+	printf "| PHP_CLI | $PHP_CLI_VERSION | | PHP_APACHE2 | $PHP_APACHE2_VERSION | | COMPOSER | $COMPOSER_VERSION |" | xargs -n5 printf '%-2s  %-15s  %-2s  %-5s %-2s\n'
+	printf "+--------------------+---------+\n\n"
 }
 
 VERBOSE_MOD=false
 PHP_CLI_VERSION=$1
 COMPOSER_VERSION=$2
 PHP_APACHE2_VERSION=$3
-
 
 while getopts ":hlv" option; do
     case $option in
@@ -81,6 +80,30 @@ fi
 
 if [ -z "$PHP_APACHE2_VERSION" ]; then 
 	PHP_APACHE2_VERSION=$PHP_CLI_VERSION
+fi
+
+if [ ! -f "/usr/bin/php$PHP_CLI_VERSION" ]; then
+	while true; do
+		read -p "install PHP CLI $PHP_CLI_VERSION (y/n): " yn
+		echo "installing php$PHP_CLI_VERSION, please wait ..."
+		case $yn in
+			[yY]) sudo apt-get -y install "php$PHP_CLI_VERSION" >> /dev/null 2>&1 && break || echo "this version of php isn't available in your depot"; exit;;
+			[nN]) exit;;
+			*) echo "invalid response";;
+		esac
+	done
+fi
+
+if [ ! -f "/etc/apache2/conf-available/php$PHP_APACHE2_VERSION-fpm.conf" ]; then
+	while true; do
+		read -p "install PHP FPM APACHE2 $PHP_APACHE2_VERSION (y/n): " yn
+		echo "installing php$PHP_APACHE2_VERSION-fpm, please wait ..."
+		case $yn in
+			[yY]) sudo apt-get -y install "php$PHP_APACHE2_VERSION-fpm" >> /dev/null 2>&1 && break || echo "this version of php fpm isn't available in your depot"; exit;;
+			[nN]) exit;;
+			*) echo "invalid response";;
+		esac
+	done
 fi
 
 Execute $VERBOSE_MOD
